e7ba3546585ec9f31fa95c8ad6a44cc88597fb62
abhinay0911 <abhinayreddy1405@gmail.com>
2025-01-04 12:33:22 +0530

    Add files via upload

---
+++ Demo5.java
@@ -0,0 +1,14 @@
+
+
+
+public class Demo5 {
+    public static void main(String[] args) {
+        String username = "abhinay";
+        String password = "abhinay0911";
+        while(username == "abhinay" && password == "abhinay0911") {
+            System.out.println("Hello user Happy new year");
+            break;
+        }
+    }
+    
+}
