1651ee6b0013d57fb2d5238e6cd493ba2d1ae9b3
abhinay0911 <abhinayreddy1405@gmail.com>
2025-01-10 13:16:42 +0530

    Add files via upload

---
+++ Test002.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Test002 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String reply;
+        while(true){
+            System.out.print("Enter your reply: ");
+            reply = sc.nextLine();
+
+            if (reply.equals("yes")){
+                System.out.println("Happy Sankranthi...");
+            } else{
+                System.out.println();
+                
+                break;
+                
+            }
+        }
+        sc.close();
+    }
+}
