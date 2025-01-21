7d7455468ff9b4fd91fe217b89430a11eee7ce90
abhinay0911 <abhinayreddy1405@gmail.com>
2025-01-21 10:10:16 +0530

    Add files via upload

--- demo016.java
+++ demo016.java
@@ -1,13 +1,15 @@
-package CoreJava;
+
+
 class PalindromeOf3 {
-	boolean isPalindromeOf3Digits(int n){
-		return (n/ 100) == (n % 10);
-	}
+    boolean isPalindromeOf3Digits(int n) {
+        return (n / 100) == (n % 10);
+    }
 }
-public class Demo016 {
+
+public class demo016 {
     public static void main(String[] args) {
         PalindromeOf3 obj = new PalindromeOf3();
-		int n = 131;	
-		System.out.println(obj.isPalindromeOf3Digits(n));
-	}
+        int n = 131;
+        System.out.println(obj.isPalindromeOf3Digits(n));
+    }
 }
--- demo017.java
+++ demo017.java
@@ -1,18 +1,18 @@
-Demo017.java   package Core_Java;
-class SumOf2Digits {
-	public static int getSumOf2Digits(int n){
-		int fd = n / 10;
-		int ld = n % 10;
-		int sum = fd + ld;
-		return sum;
-	}
+
+
+class SumOftwoDigits {
+    public static int getSumOftwoDigits(int n) {
+        int a = n / 10;
+        int b = n % 10;
+        int sum = a + b;
+        return sum;
+    }
 }
 
-public class Demo017 {
+public class demo017 {
     public static void main(String[] args) {
-        SumOf2Digits obj = new SumOf2Digits();
-		int n = 78;
-		int sum = obj.getSumOf2Digits(n);
-		System.out.println("The sum of 2 digits is : "+sum);
-	}
+        int n = 78;
+        int sum = SumOftwoDigits.getSumOftwoDigits(n);
+        System.out.println("The sum of two digits is : " + sum);
+    }
 }
