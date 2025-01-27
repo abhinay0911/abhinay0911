0d455d94e215ae40d89a618d8ba1d03e9ba90fec
abhinay0911 <abhinayreddy1405@gmail.com>
2025-01-27 11:05:07 +0530

    Add files via upload

--- Test002.java
+++ Test002.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Test002 {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String reply;
