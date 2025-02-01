6ecb7bb46614f3e88404e8d93d7ec3e0ff7dfdc9
abhinay0911 <abhinayreddy1405@gmail.com>
2025-02-01 10:26:25 +0530

    print numbers from 1 to 10

---
+++ Demo004.java
@@ -0,0 +1,11 @@
+public class Demo004 {
+
+    //print numbers from 1 t0 10 
+    public static void main(String[] args) {
+        for(int i=0;i<=10;i++){
+            System.out.println(i);
+        }
+        
+    }
+    
+}
