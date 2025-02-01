4557c31ae6f13cf03684cc7c18ef2146d7c16036
abhinay0911 <abhinayreddy1405@gmail.com>
2025-02-01 10:13:48 +0530

    Plus One

--- Demo007.java
+++ Demo007.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
-
-public class Demo7 {
+public class Demo007 {
+    
+    // printing user details
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         System.out.print("please enter your name:");
