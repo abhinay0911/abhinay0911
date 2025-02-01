d376bb02af20147397f20dd53ecce65cb6dd64d3
abhinay0911 <abhinayreddy1405@gmail.com>
2025-02-01 10:27:06 +0530

    username and password

---
+++ Demo005.java
@@ -0,0 +1,14 @@
+
+public class Demo005 {
+
+    // happy new year to user
+    public static void main(String[] args) {
+        String username = "abhinay";
+        String password = "abhinay0911";
+        while(username == "abhinay" && password == "abhinay0911") {
+            System.out.println("Hello user Happy new year");
+            break;
+        }
+    }
+    
+}
