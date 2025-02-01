510aaf288fbbcfa18417a183f3f1b56528a81b4f
abhinay0911 <abhinayreddy1405@gmail.com>
2025-02-01 10:59:01 +0530

    user and pass

---
+++ Demo006.java
@@ -0,0 +1,10 @@
+public class Demo006 {
+    public static void main(String[] args){
+        String username = "Abhinay_0911";
+        String pass = "abhinay";
+        while((username == "Abhinay_0911") && (pass == "abhinay")){
+            System.out.println("Hello Abhinay_0911 Welcome to malla Reddy University");
+            break;
+        }
+    }
+}
